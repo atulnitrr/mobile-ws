c147222b23618965a33f037d370601e9a0aa513d
atulnitrr <atulkumar458@gmail.com>
2019-07-01 12:57:52 +0530

    Added request params

--- src/main/java/com/akumar/ws/mobilews/controller/UserController.java
+++ src/main/java/com/akumar/ws/mobilews/controller/UserController.java
@@ -7,15 +7,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @CrossOrigin
-@RequestMapping("users")
+@RequestMapping("/users")
 public class UserController {
 
 
+    // Query string or Request param example
+    // http://localhost:8080/users?limit=20&page=2
+    @GetMapping
+    public String getUsers(@RequestParam(value ="page", defaultValue = "1") int page,
+            @RequestParam(value = "limit", defaultValue = "25") int limit)
+    {
+        return "Get users for page  " + page + " limit  " + limit;
+    }
+
+
+    // path parameter or path variable example
+//    http://localhost:8080/users/890
     @GetMapping("/{userId}")
     public String getUser(@PathVariable final String userId) {
         return "Get user was called with userId " + userId;
