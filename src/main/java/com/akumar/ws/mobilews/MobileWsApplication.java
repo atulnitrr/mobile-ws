81fd2fcebaec1deaf0a0b9a04e57e407dc212677
atulnitrr <atulkumar458@gmail.com>
2019-07-01 11:48:18 +0530

    Initial commit

---
+++ src/main/java/com/akumar/ws/mobilews/MobileWsApplication.java
@@ -0,0 +1,14 @@
+package com.akumar.ws.mobilews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileWsApplication.class, args);
+		System.out.println("Spring boot app is running tada -->ß");
+	}
+
+}
