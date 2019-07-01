b54924c557fbddb7d63a0ae1a5d544719d10ef9e
atulnitrr <atulkumar458@gmail.com>
2019-07-01 12:22:04 +0530

    Read path variable

--- src/main/java/com/akumar/ws/mobilews/controller/UserController.java
+++ src/main/java/com/akumar/ws/mobilews/controller/UserController.java
@@ -3,6 +3,7 @@ package com.akumar.ws.mobilews.controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
 
-    @GetMapping
-    public String getUser() {
-        return "Get user was called";
+    @GetMapping("/{userId}")
+    public String getUser(@PathVariable final String userId) {
+        return "Get user was called with userId " + userId;
     }
 
     @PostMapping
