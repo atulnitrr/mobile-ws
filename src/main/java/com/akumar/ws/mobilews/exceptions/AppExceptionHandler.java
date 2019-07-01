603a313fbacec993186670494f063d445334d4b4
atulnitrr <atulkumar458@gmail.com>
2019-07-01 16:27:36 +0530

    Handle custom exceptions

--- src/main/java/com/akumar/ws/mobilews/exceptions/AppExceptionHandler.java
+++ src/main/java/com/akumar/ws/mobilews/exceptions/AppExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.akumar.ws.mobilews.exceptions;
 
+import java.util.Date;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import com.akumar.ws.mobilews.modal.response.ErrorMessage;
 
 
 @ControllerAdvice
@@ -14,6 +16,19 @@ public class AppExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {Exception.class})
     public ResponseEntity<Object> handleAnyException(final Exception ex, final WebRequest webRequest) {
-        return new ResponseEntity<>(ex, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+
+        final String message = ( ex.getLocalizedMessage() != null ? ex.getLocalizedMessage() : ex.toString());
+        final ErrorMessage errorMessage = new ErrorMessage(new Date(), message);
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value = {NullPointerException.class})
+    public ResponseEntity<Object> handleNullPointerException(final NullPointerException ex,
+            final WebRequest webRequest) {
+
+        final String message = ( ex.getLocalizedMessage() != null ? ex.getLocalizedMessage() : ex.toString()) +
+                "--> testing";
+        final ErrorMessage errorMessage = new ErrorMessage(new Date(), message);
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
---
+++ src/main/java/com/akumar/ws/mobilews/modal/response/ErrorMessage.java
@@ -0,0 +1,34 @@
+package com.akumar.ws.mobilews.modal.response;
+
+import java.util.Date;
+
+
+public class ErrorMessage {
+
+    private Date timeStamp;
+    private String message;
+
+    public ErrorMessage() {
+    }
+
+    public ErrorMessage(final Date timeStamp, final String message) {
+        this.timeStamp = timeStamp;
+        this.message = message;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(final Date timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(final String message) {
+        this.message = message;
+    }
+}
